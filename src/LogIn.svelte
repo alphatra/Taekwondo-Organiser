26250e0cbe4364258035ca1388229e2b3476af24
Kamici <mrnikrutix@gmail.com>
2019-06-27 14:39:24 +0200

    Add Login page without back-end

---
+++ src/LogIn.svelte
@@ -0,0 +1,17 @@
+<script>
+	let user = { loggedIn: false };
+
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+</script>
+
+{#if user.loggedIn}
+	<button class="uk-button uk-button-primary uk-border-pill uk-width-1-1" on:click={toggle}>
+		Log out
+	</button>
+{:else}
+	<button class="uk-button uk-button-primary uk-border-pill uk-width-1-1" on:click={toggle}>
+		Log in
+	</button>
+{/if}
--- src/Menu.svelte
+++ src/Menu.svelte
@@ -2,6 +2,7 @@
   import 'bulma/css/bulma.css'
   import 'svelma'
   import '@fortawesome/fontawesome-free/css/all.css'
+  import LogIn from './LogIn.svelte';S
   export let  generator;
   function handleClick() {
     generator = 0
@@ -22,6 +23,7 @@
 <div class="column is-3">
   <aside class="menu" style="padding-top: 100px">
   <ul class="menu-list">
+  <LogIn />
     <a id="page1" class="is-large is-fullwidth is-white is-size-4 navbar-item "  style="padding-top: 20px; padding-bottom:20px" on:click={handleClick}>Calendar</a>
     <li><a id="page2" class="is-large is-fullwidth is-white is-size-4 navbar-item "  style="padding-top: 20px; padding-bottom:20px" on:click={handleClick1}>Database of affiliated persons</a></li>
     <li><a id="page3" class="is-large is-fullwidth is-white is-size-4 navbar-item "  style="padding-top: 20px; padding-bottom:20px" on:click={handleClick2}>Database of clubs</a></li>
